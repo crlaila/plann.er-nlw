73cac6030d2aa1151c699efbe59f455fcde5ee9e
Laila Silva <laila.cristine@outlook.com>
2024-07-12 23:06:20 -0300

    criando modal para adicionar/remover emails

--- src/app.tsx
+++ src/app.tsx
@@ -1,66 +1,167 @@
 // importando ícones da biblioteca lucide-react
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2} from "lucide-react";
-import { useState } from "react";
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus} from "lucide-react";
+import { useState, type FormEvent } from "react";
 
 export function App() {
   const [isGuestInputOpen, setIsGuestInputOpen] = useState(false)
+  const [isGuestModalOpen, setIsGuestModalOpen] = useState(false)
+  const [emailsToInvite, setEmailsToInvite] = useState([
+    'laila.cristine@outlook.com'
+  ])
 
+  //função para fechamento e abertura de input
   function openGuestInput(){
     setIsGuestInputOpen(true)
   }
+  function closeGuestInput(){
+    setIsGuestInputOpen(false)
+  }
+  //função para fechamento e abertura de modal
+  function openGuestModal(){
+    setIsGuestModalOpen(true)
+  }
+  function closeGuestModal(){
+    setIsGuestModalOpen(false)
+  }
+
+  //função para adicionar novo e-mail, coloca o evento e adiciona o prevent para que a página não recarregue
+  function addNewEmailToInvite(event: FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    
+    const data = new FormData(event.currentTarget)
+    const email = data.get('email')?.toString()
+
+    //if para quando o usuário não preencher nada no e-mail
+    if (!email){
+      return
+    }
+
+    //usado para não ter e-mail repetido
+    if (emailsToInvite.includes(email)){
+      return
+    }
+
+    //usado para enviar as informações para o array, o useState é imutável, então para isso tem que criar um novo array
+    setEmailsToInvite([
+      ...emailsToInvite,
+      email
+    ])
+
+    //depois que o usuário preencher reseta o form
+    event.currentTarget.reset()
+  }
+
+  //funcao para excluir o email adicionado
+  function removeEmailFromInvites(emailToRemove: string){
+    const newEmailList = emailsToInvite.filter(invited => invited !== emailToRemove)
+    setEmailsToInvite(newEmailList)
+  }
+
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
         <div className="flex flex-col items-center gap-3">
           <img src="/logo.svg" alt="plann.er" />
           <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
-      </div>
-        
-      <div>
-        <div className="h-16 bg-zinc-900 px-4 flex items-center rounded-xl shadow-shape gap-3">
-          <div className="flex items-center gap-2 flex-1">
-            <MapPin className="size-5 text-zinc-400" />
-            <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
-          </div>
-          <div className="flex items-center gap-2">
-            <Calendar className="size-5 text-zinc-400" />
-            <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"/>
-          </div>
-
-          <div className="w-px h6 bg-zinc-800" />
-            {isGuestInputOpen ? (
-              <button className="bg-lime-800 text-lime-200 rounded-lg flex items-center gap-2 px-5 py-2 font-medium hover:bg-zinc-700">
-                Alterar local/data
-              <Settings2 className="size-5" />
-              </button>
-            ) : (
-              <button onClick={openGuestInput} className="bg-lime-300 text-lime-950 rounded-lg flex items-center gap-2 px-5 py-2 font-medium hover:bg-lime-400">
-                Continuar
-              <ArrowRight className="size-5" />
-              </button>
-            )}
         </div>
+          
+        <div>
+          <div className="h-16 bg-zinc-900 px-4 flex items-center rounded-xl shadow-shape gap-3">
+            <div className="flex items-center gap-2 flex-1">
+              <MapPin className="size-5 text-zinc-400" />
+              <input disabled={isGuestInputOpen} type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
+            </div>
+            <div className="flex items-center gap-2">
+              <Calendar className="size-5 text-zinc-400" />
+              <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"/>
+            </div>
+
+            <div className="w-px h6 bg-zinc-800" />
+              {isGuestInputOpen ? (
+                <button onClick={closeGuestInput} className="bg-lime-800 text-lime-200 rounded-lg flex items-center gap-2 px-5 py-2 font-medium hover:bg-zinc-700">
+                  Alterar local/data
+                <Settings2 className="size-5" />
+                </button>
+              ) : (
+                <button onClick={openGuestInput} className="bg-lime-300 text-lime-950 rounded-lg flex items-center gap-2 px-5 py-2 font-medium hover:bg-lime-400">
+                  Continuar
+                <ArrowRight className="size-5" />
+                </button>
+              )}
+          </div>
+        </div>
+        
+        {isGuestInputOpen && (
+          <div className="h-16 bg-zinc-900 px-4 flex items-center rounded-xl shadow-shape gap-3 space-y-0">
+            <button type="button" onClick={openGuestModal} className="flex items-center gap-2 flex-1 text-left">
+              <UserRoundPlus className="size-5 text-zinc-400" />
+              <span className="text-zinc-400 text-lg text-left">Quem estará na viagem?</span>
+              <input type="text" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
+            </button>
+
+            <div className="w-px h6 bg-zinc-800"></div>
+
+            <button className="bg-lime-300 text-lime-950 rounded-lg flex items-center gap-2 px-5 py-2 font-medium hover:bg-lime-400">
+              Confirmar viagem
+              <ArrowRight className="size-5" />
+            </button>
+          </div>
+        )}
+        
+        <p className="text-sm text-zinc-500">Ao planejar sua viagem pela plann.er você automaticamente concorda < br />
+        com nossos <a className ="text-zinc-300 underline" href="#">termos de uso</a> e <a className ="text-zinc-300 underline" href="#">políticas de privacidade.</a></p>
       </div>
       
-      {isGuestInputOpen && (
-        <div className="h-16 bg-zinc-900 px-4 flex items-center rounded-xl shadow-shape gap-3 space-y-0">
-          <div className="flex items-center gap-2 flex-1">
-            <UserRoundPlus className="size-5 text-zinc-400" />
-            <input type="text" placeholder="Quem estará na viagem?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
+      {isGuestModalOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+           <div className="space-y-2">
+            <div className="flex items-center justify-between">
+                <h2 className="texr-lg font-semibold">Selecionar convidados</h2>
+                <button type="button" onClick={closeGuestModal}>
+                  <X className="size-5 text-zinc-400" />
+                </button>
+              </div>
+              <p className="text-sm text-zinc-400">
+                Os convidados irão receber e-mails para confirmar a participação na viagem.
+              </p>
+            </div>
+
+            <div className="flex flex-wrap gap-2">
+              {emailsToInvite.map(email =>{
+                //map para percorrer array
+                return(
+                  //key obrigatório para react
+                  <div key={email} className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
+                    <span className="text-zinc-300">{email}</span>
+                    <button type="button" onClick={() => removeEmailFromInvites(email)}>
+                      <X className="size-4 text-zinc-400" />
+                    </button>
+                  </div>
+                )
+              })}
+            </div>
+
+            <div className="w-full h-px bg-zinc-800" />
+
+            <form onSubmit={addNewEmailToInvite} className="p-2.5 bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
+              <div className="px-2 flex items-center flex-1 gap-2">
+                <AtSign className="text-zinc-400 size-5" />
+                <input 
+                type="type" 
+                name="email" 
+                placeholder="Digite o e-mail do convidado" 
+                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                /> 
+              </div>
+                <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg flex items-center gap-2 px-5 py-2 font-medium hover:bg-lime-400">
+                Convidar
+                <Plus className="size-5" />
+              </button>
+            </form>
           </div>
-
-          <div className="w-px h6 bg-zinc-800"></div>
-
-          <button className="bg-lime-300 text-lime-950 rounded-lg flex items-center gap-2 px-5 py-2 font-medium hover:bg-lime-400">
-            Confirmar viagem
-            <ArrowRight className="size-5" />
-          </button>
         </div>
       )}
-      
-      <p className="text-sm text-zinc-500">Ao planejar sua viagem pela plann.er você automaticamente concorda < br />
-      com nossos <a className ="text-zinc-300 underline" href="#">termos de uso</a> e <a className ="text-zinc-300 underline" href="#">políticas de privacidade.</a></p>
-    </div>
     </div>
   )
 }
